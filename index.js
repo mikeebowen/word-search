1b85db8f8f7b7693f8c1fc82a3ea34750da2302d
Michael Bowen <mikee.bowen@gmail.com>
2015-10-09 20:48:30 -0700

    add latest

--- index.js
+++ index.js
@@ -2,10 +2,47 @@
 
 var express = require('express');
 var app = express();
+var easyAsync = require('easy-async');
+var fs = require('fs');
 var port = process.env.PORT || 3000;
 var time = new Date();
+
+function getFileData (callback) {
+  var output;
+  fs.readFile('./WordSearch.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+    console.log(data);
+    return data.toString(data);
+    callback();
+  });
+  // console.log(output);
+  // return output;
+}
+
+function makeCallback (callback) {
+  console.log('info');
+  callback();
+}
+easyAsync.start(getFileData)
+.then(makeCallback);
+// var exampleTask = function(callback){
+//   var count = 1;
+//   console.log('starting task', count);
+//   setTimeout(function(){
+//     console.log('done with task' count);
+//     count++;
+//     callback();
+//   }, 1000);
+// }
+
+// easyAsync.start(exampleTask)
+// .then(exampleTask)
+// .then(exampleTask)
+// .then(function() {
+//   console.log('continuing after tasks in series');
+// });
+
 
 app.listen(port, function () {
   console.log('Server started on port: ' + port + ', at ' + time);
 });
-git
