f14c085598685d09e46f1c04afc2334b685d5e27
Michael Bowen <mikee.bowen@gmail.com>
2015-10-18 11:55:13 -0700

    clean up code

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ var port = process.env.PORT || 3000;
 var time = new Date();
 var solveWordSearch = require('./lib/solve-word-search');
 
-var theAnswers = solveWordSearch('./WordSearch.txt', './WordList.txt');
-
-console.log(theAnswers);
+solveWordSearch('./WordSearch.txt', './WordList.txt');
 
 app.listen(port, function () {
   console.log('Server started on port: ' + port + ', at ' + time);
