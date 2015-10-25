a5de2c4b5d691730bc16f4ea93ffdac0ef3b1c6f
Michael Bowen <mikee.bowen@gmail.com>
2015-10-24 18:56:52 -0700

    add front end for testing your own files

--- routes/word-search-routes.js
+++ routes/word-search-routes.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var path = require('path');
 var bodyparser = require('body-parser');
+var findWords = require('../lib/solve-word-search');
 
 module.exports = function (router) {
   router.use(bodyparser.json());
@@ -10,10 +11,35 @@ module.exports = function (router) {
   extended: true
 }));
 
-  router.route('/searchwords')
-    .post(function (req, res) {
-      console.log(req.file);
+  router.route('/')
+    .get(function (req, res) {
+      res.sendFile(path.join(__dirname + '/index.html'));
+    })
+/*    .post(function (req, res) {
+      fs.readFile(req.body.words, function (err, data) {
+        var theWords = data;
+        fs.readFile(req.body.wordBlock, function (err, data2) {
+          var theWordBlock = data2;
+          fs.readFile(path.join(__dirname, '../index.html'), function (err, data3) {
+            var theHtml = data3.toString();
+            var theAnswers = '';
+            findWords(theWordBlock, theWords);
+          })
+        })
+      });
+      // console.log(txt);
       res.redirect('back');
+    })*/
+    .post(function (req, res) {
+      findWords(req.body.wordBlock, req.body.words, function (data) {
+        fs.readFile(path.join(__dirname, '../index.html'), function (err, data2) {
+          var theHtml = data2.toString('');
+          var hF = data.horizontallyForwards = data.horizontallyForwards.join(', ');
+          theHtml = theHtml.replace('<div id="theAnswers"></div>', '<div id="theAnswers"><h5>Horizontally Forward</h5><p>' + hF + '</p><h5>Horizontally Backwards</h5><p>' + data.horizontallyBackwards.join(', ') + '</p><h5>Vertically Forwards</h5><p>' + data.verticallyForwards.join(', ') + '</p><h5>Vertically Backwards</h5><p>' + data.verticallyBackwards.join(', ') + '</p><h5>Diagonally Forwards</h5><p>' + data.diagonallyForwards.join(', ') + '</p><h5>Diagonally Forwards</h5><p>' + data.diagonallyBackwards.join(', ') + '</p></div>');
+          res.send(theHtml);
+
+        })
+      });
     })
 
 }
